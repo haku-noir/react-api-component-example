d210a2c7e3adca6bd1ff2a2a2646741caf1cd2ef
haku-noir <hakunoir.00@gmail.com>
2020-07-31 04:59:55 +0900

    add: UpdateTweetsAPIにコメントを追加

--- app/src/api/UpdateTweetsAPI.tsx
+++ app/src/api/UpdateTweetsAPI.tsx
@@ -5,6 +5,9 @@ import { RootState } from 'store';
 import { entitiesActions } from 'actions/entitiesActions';
 import { tweetsAPIActions } from 'actions/tweetsAPIActions';
 
+/*
+  サーバAPIとの通信用クライアント
+*/
 const fetchTweets = () => fetch('http://localhost/tweets', {
   method: 'GET',
   mode: 'cors',
@@ -14,6 +17,10 @@ const fetchTweets = () => fetch('http://localhost/tweets', {
 export const UpdateTweetsAPI: React.FC<{}> = () => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
+  /*
+    tweetsAPIReducerが管理するupdatingプロパティ(boolean型)
+    非同期処理開始のトリガーとなるAPIReducerが管理するプロパティ
+  */
   const updating = useSelector<RootState, boolean>(
     state => state.tweetsAPI.updating
   );
@@ -21,19 +28,26 @@ export const UpdateTweetsAPI: React.FC<{}> = () => {
   React.useEffect(() => {
     if(!updating) return;
 
+    // 2. サーバからツイート全体を取得
     fetchTweets()
       .then(res => res.json())
       .then(res => {
         if(!res.tweets) return;
+
+        // 3. entitiesReducerが管理するtweetsプロパティに取得したツイート全体を保存
         dispatch(entitiesActions.updateTweets(res.tweets));
       })
       .then(() => {
+        /*
+          非同期処理の終了をディスパッチ
+          データの流れでは省略
+        */
         dispatch(tweetsAPIActions.updateTweetsDone());
       })
       .catch(() => {
         dispatch(tweetsAPIActions.updateTweetsDone());
       });
-  }, [updating]);
+  }, [updating]); // 1. updatingプロパティの更新を検知
 
   return null;
 };
